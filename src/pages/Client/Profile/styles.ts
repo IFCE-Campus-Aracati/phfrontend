cad99181034203c1a75c6721b87254dfe546e162
allinegsPY <allinegd80@gmail.com>
2023-04-05 17:23:42 -0300

    Tela de perfil e editar impressão atualizadas

--- src/pages/Client/Profile/styles.ts
+++ src/pages/Client/Profile/styles.ts
@@ -8,27 +8,27 @@ export const Container = styled.div`
   position: relative;
   overflow: auto;
   height: 100vh;
-  width: 110vw;
+  width: 100%;
 `;
 
 export const Title = styled.h1`
   color: ${theme.colors.white};
   position: absolute;
   top: 35px;
-  font-size: 2rem;
-  margin-left: 6.5rem;
+  font-size: 1.5rem;
+  margin-left: 1.5rem;
 `;
 
 export const SideBar = styled.div`
+  position: fixed;
 `;
 
 export const FormContainer = styled.div`
   background-color: ${theme.colors.background.secondary};  
   position: absolute;
-  left: 80px;
   top: 80px;
   padding: 2rem;
-  width: 82%;
+  width: 88vw;
   height: 80%;
   border-radius: 8px;
   text-align: top;
@@ -37,7 +37,7 @@ export const FormContainer = styled.div`
 
 export const TitleInput = styled.h2`
   color: ${theme.colors.white};
-  font-size: 1.5rem;
+  font-size: 1rem;
   margin-bottom: 1.4rem;
   margin-top: 0.25rem;
   text-align: left;
@@ -47,7 +47,7 @@ export const Subtitle = styled.h3`
   color: ${theme.colors.white};
   font-style: normal;
   font-weight: 400;
-  font-size: 1rem;
+  font-size: 0.8rem;
   line-height: 1.4rem;
   margin-bottom: 2.5rem;
   text-align: left;
@@ -66,22 +66,21 @@ export const ImageSide = styled.img`
   height: 50vh;
   width: 20vw;
   object-fit: cover;
-  transform: translate(60%, -55%);
+  margin-left: 10rem;
+  margin-top: -10rem;
 `;
 
 export const Attachments = styled.div`
   position: absolute;
-  left: 54%;
   display: flex;
   flex-direction: row;
   gap: 1.0rem;
-  width: 100%;
   padding: 1rem;
-  margin-left: 7.5rem;
-  margin-bottom: 3rem;
-  transform: translate(1%, -250%);
+  margin-left: 50.5rem;
+  margin-top: 29rem;
 `;
 
 export const Pass = styled.div`
-  transform: translate(10%, -240%);
+  margin-left: 8rem;
+  margin-top: -6rem;
 `;
